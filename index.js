328596cd05da13ee98ce09b1704ebfe5165a0409
Kazuki Imamura <kaizumaki@gmail.com>
2022-11-28 10:57:28 +0900

    [WIP]toioを動かすボタンを２パターンにする

--- index.js
+++ index.js
@@ -7,7 +7,12 @@
 const OBNIZ_ID = '6119-5084'
 const TOIO_SERVICE_UUID = '10b20100-5b3b-4571-9508-cf3efcd7bbae'
 const MOTOR_CHARACTERISTIC_UUID = '10b20102-5b3b-4571-9508-cf3efcd7bbae'
-const motorBuf = new Uint8Array([0x02, 0x01, 0x01, 0x32, 0x02, 0x02, 0x32, 0x78])
+const SOUND_CHARACTERISTIC_UUID = '10B20104-5B3B-4571-9508-CF3EFCD7BBAE'
+const motorBuf1 = new Uint8Array([0x02, 0x01, 0x01, 0x32, 0x02, 0x02, 0x32, 0x78])
+const soundBuf1 = new Uint8Array([0x02, 0x04, 0xFF])
+const motorBuf2 = new Uint8Array([0x02, 0x01, 0x01, 0x32, 0x02, 0x02, 0x32, 0x78])
+const soundBuf2 = new Uint8Array([0x02, 0x06, 0xFF])
+
 
 function connect() {
   const obniz = new Obniz(OBNIZ_ID)
@@ -49,16 +54,38 @@ function connect() {
 
     await obniz.ble.scan.startWait(target)
 
-    $('#move-toio').click(async function () {
-      console.log('move toio')
-      $('#status').text('move toio')
+    $('#move-toio1').click(async function() {
+      console.log('move toio pattern1')
+      $('#status').text('move toio pattern1')
       const peripheralList = obniz.ble.getConnectedPeripherals()
       if (peripheralList.length > 0) {
         for (const peripheral of peripheralList) {
           await peripheral
             .getService(TOIO_SERVICE_UUID)
+            .getCharacteristic(SOUND_CHARACTERISTIC_UUID)
+            .writeWait(soundBuf1)
+          await peripheral
+            .getService(TOIO_SERVICE_UUID)
             .getCharacteristic(MOTOR_CHARACTERISTIC_UUID)
-            .writeWait(motorBuf)
+            .writeWait(motorBuf1)
+        }
+      }
+    })
+
+    $('#move-toio2').click(async function() {
+      console.log('move toio pattern2')
+      $('#status').text('move toio pattern2')
+      const peripheralList = obniz.ble.getConnectedPeripherals()
+      if (peripheralList.length > 0) {
+        for (const peripheral of peripheralList) {
+          await peripheral
+              .getService(TOIO_SERVICE_UUID)
+              .getCharacteristic(SOUND_CHARACTERISTIC_UUID)
+              .writeWait(soundBuf2)
+          await peripheral
+              .getService(TOIO_SERVICE_UUID)
+              .getCharacteristic(MOTOR_CHARACTERISTIC_UUID)
+              .writeWait(motorBuf2)
         }
       }
     })
